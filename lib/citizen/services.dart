898a55203848884fd15f839d747b627f19e0c7d8
TybcaGroup <khadijamhmama@gmail.com>
2024-03-02 16:17:36 +0530

    Service Page updated1

--- lib/citizen/services.dart
+++ lib/citizen/services.dart
@@ -14,12 +14,18 @@ class _ServicePageState extends State<ServicePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text('Services'),
+        title: Text('Services',
+        style:TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Color(0xFFF8F8F8)
+        ),
+        ),
         centerTitle: true,
         elevation: 0,
         automaticallyImplyLeading: false,
       ),
-      body: Column(
+        body: SingleChildScrollView(
+        child: Column(
         children: [
           Padding(
             padding: const EdgeInsets.only(
@@ -52,7 +58,7 @@ class _ServicePageState extends State<ServicePage> {
                   padding: EdgeInsets.all(10),
                   width: MediaQuery.of(context).size.width - 20,
                   decoration: BoxDecoration(
-                    color: Color(0xFF90CAF9),
+                    color: Color(0xFFFFCC99),
                     borderRadius: BorderRadius.circular(10), // make the border circular
                   ),
                   child: Text(
@@ -60,7 +66,7 @@ class _ServicePageState extends State<ServicePage> {
                     style: GoogleFonts.laila(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Color(0xFF1565C0),
+                      color: Color(0xFFCC6600),
                     ),
                   ),
                 ),
@@ -74,16 +80,22 @@ class _ServicePageState extends State<ServicePage> {
             width: MediaQuery.of(context).size.width - 20,
             padding: EdgeInsets.all(10),
             decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFFFCC99), // Change this to the color you want
+                width: 1, // Change this to the width you want
+              ),
               borderRadius: BorderRadius.all(Radius.circular(10)),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            child: GridView.count(
+              crossAxisCount: 2,
               children: [
                 Column(
                   children: [
+                    SizedBox(height: 40),
                     Align(
                       alignment: Alignment.bottomCenter,
-                      child: Image.asset('images/sos.png', scale: 3.5),
+                      child: Image.asset('images/img.png', scale: 6.0),
                     ),
                     SizedBox(height: 10), // add some space between the image and the text
                     Center(
@@ -93,9 +105,10 @@ class _ServicePageState extends State<ServicePage> {
                 ),
                 Column(
                   children: [
+                    SizedBox(height: 40),
                     Align(
                       alignment: Alignment.bottomCenter,
-                      child: Image.asset('images/em1.png', scale: 3.7),
+                      child: Image.asset('images/em1.png', scale: 6.0),
                     ),
                     SizedBox(height: 10), // add some space between the image and the text
                     Center(
@@ -106,8 +119,260 @@ class _ServicePageState extends State<ServicePage> {
               ],
             ),
           ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 10), // add padding to the left side
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF66FFFF),
+                    borderRadius: BorderRadius.circular(10), // make the border circular
+                  ),
+                  child: Text(
+                    "Citizen Safety Services",
+                    style: GoogleFonts.laila(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF1565C0),
+                    ),
+                  ),
+                ),
+
+              ),
+            ],
+          ),
+          // 10 logical pixels of space
+          Container(
+            height: 350,
+            width: MediaQuery.of(context).size.width - 20,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFF66FFFF), // Change this to the color you want
+                width: 1, // Change this to the width you want
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/img.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Report Offence'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Find police Station'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Find Hospital'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 10), // add padding to the left side
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFCC99FF),
+                    borderRadius: BorderRadius.circular(10), // make the border circular
+                  ),
+                  child: Text(
+                    "Report To Us",
+                    style: GoogleFonts.laila(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF990099),
+                    ),
+                  ),
+                ),
+
+              ),
+            ],
+          ),
+          // 10 logical pixels of space
+          Container(
+            height: 350,
+            width: MediaQuery.of(context).size.width - 20,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFFCC99FF), // Change this to the color you want
+                width: 1, // Change this to the width you want
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/img.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Report Offence'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Report Cyber fraud'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Share information Anonymously'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 10), // add padding to the left side
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF99FFCC),
+                    borderRadius: BorderRadius.circular(10), // make the border circular
+                  ),
+                  child: Text(
+                    "Information Services",
+                    style: GoogleFonts.laila(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF009900),
+                    ),
+                  ),
+                ),
+
+              ),
+            ],
+          ),
+          // 10 logical pixels of space
+          Container(
+            height: 350,
+            width: MediaQuery.of(context).size.width - 20,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Color(0xFF99FFCC), // Change this to the color you want
+                width: 1, // Change this to the width you want
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+
+            child: GridView.count(
+              crossAxisCount: 2,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/img.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Self Defence Videos'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Cyber Crime Information'),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    SizedBox(height: 40),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset('images/em1.png', scale: 6.0),
+                    ),
+                    SizedBox(height: 10), // add some space between the image and the text
+                    Center(
+                      child: Text('Internet Tips'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+
         ],
       ),
+    ),
     );
   }
 }
