bfa92cbd6afb837e57bae50c9cbd0778eabbe42c
TybcaGroup <khadijamhmama@gmail.com>
2024-03-06 21:39:34 +0530

    new signup page

---
+++ lib/Pages/signup.dart
@@ -0,0 +1,161 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final nameController=TextEditingController();
+  final emailController=TextEditingController();
+  final mobileController=TextEditingController();
+  final passwordController=TextEditingController();
+  final _formkey=GlobalKey<FormState>();
+  bool _obscureText =true ;
+  String? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+
+        child: Container(
+        height: MediaQuery.of(context).size.height,
+    width: MediaQuery.of(context).size.width,
+    decoration: BoxDecoration(
+    gradient: LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+    Color(0xFFFF0000),
+      Color(0xFF90CAF9),
+      Color(0xFF1565C0),
+      Color(0xFF7B0305),
+
+    ]),
+
+    ),
+    child: Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Form(
+    key: _formkey,
+    child: Column(
+    children: [
+      TextFormField(
+    controller: nameController,
+    decoration: const InputDecoration(
+    hintText: 'Name',
+    prefixIcon: Icon(Icons.person),
+    ),
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+    return 'Please enter your name';
+    }
+    return null;
+    },
+    ),
+      TextFormField(
+        controller: emailController,
+        decoration: const InputDecoration(
+          hintText: 'E-Mail',
+          prefixIcon: Icon(Icons.email),
+        ),
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+    return 'Please enter your email';
+    }
+    if (!RegExp(r'^[\w-]+(\.[\w-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}').hasMatch(value)) {
+    return 'Please enter a valid email address';
+    }
+    return null;
+    },
+      ),
+      TextFormField(
+        controller: mobileController,
+        decoration: const InputDecoration(
+          hintText: 'Mobile Number',
+          prefixIcon: Icon(Icons.phone),
+        ),
+    inputFormatters: <TextInputFormatter>[
+    FilteringTextInputFormatter.digitsOnly,
+    LengthLimitingTextInputFormatter(10),
+    ],
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+    return 'Please enter your mobile number';
+    }
+    if (value.length != 10) {
+    return 'Please enter a valid 10-digit mobile number';
+    }
+    return null;
+    },
+      ),
+
+
+      DropdownButtonFormField<String>(
+      decoration: const InputDecoration(
+      hintText: 'Gender',
+      prefixIcon: Icon(Icons.person),
+    ),
+      value: selectedGender,
+      onChanged: (value) {
+        setState(() {
+          selectedGender = value;
+        });
+      },
+      items: ['Male', 'Female', 'Other']
+          .map((gender) => DropdownMenuItem<String>(
+        value: gender,
+        child: Text(gender),
+      ))
+          .toList(),
+    ),
+      TextFormField(
+        controller: passwordController,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          prefixIcon: Icon(Icons.lock),
+          suffixIcon: IconButton(
+            icon: _obscureText
+                ? Icon(Icons.visibility_off)
+                : Icon(Icons.visibility),
+            onPressed: () {
+              setState(() {
+                _obscureText = !_obscureText;
+              });
+            },
+          ),
+        ),
+        obscureText: _obscureText,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your password';
+          }
+          return null; // Return null if the password is not empty
+        },
+      ),
+      const SizedBox(height: 35),
+      ElevatedButton(
+          onPressed: (){
+            CollectionReference collref=FirebaseFirestore.instance.collection('signup');
+            collref.add({
+              'name':nameController.text,
+              'email':emailController.text,
+              'mobile':mobileController.text,
+              'gender':selectedGender,
+              'password':passwordController.text,
+            });
+          }, child: const Text('SignUp'),
+      )
+    ],
+    ),
+    ),
+    ),
+        ),
+        ),
+    );
+  }
+}
