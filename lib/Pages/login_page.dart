f3493105769aad6ab657c35a4c40070331b4df1e
TybcaGroup <khadijamhmama@gmail.com>
2024-03-12 21:32:29 +0530

    login btn change

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -137,7 +137,7 @@ class LoginPage extends StatefulWidget {
                             )
                         ) ,
                         child: Padding(padding: EdgeInsets.fromLTRB(15, 8, 15, 8),
-                          child: Text('Click this to login',
+                          child: Text('Click on this button to login',
                             style: TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
