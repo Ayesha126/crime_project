b3f9f7df455f94bf783c968ecd6ab952553887c7
TybcaGroup <khadijamhmama@gmail.com>
2024-03-21 23:48:31 +0530

    information services completed

--- lib/citizen/services pages/information services/cybersecurity.dart
+++ lib/citizen/services pages/information services/cybersecurity.dart
@@ -4,40 +4,76 @@ class CyberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFF90CAF9),
-          title: Text('Cyber Security Information',
-            style:TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color(0xFFF8F8F8)
-            ),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF90CAF9),
+        title: Text(
+          'Cyber Security Information',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFF8F8F8),
           ),
-          centerTitle: true,
-          elevation: 0,
-          automaticallyImplyLeading: false,
         ),
-        body: Padding(
-        padding: const EdgeInsets.all(4.0),
-    child: SingleChildScrollView(
-    child: Text(
-      '''  Cyber Security Norms and Best Practices
-      1. Always use genuine software.
-      2. Install the latest updates /patches for the operating sysytem,Antivirus and Application software.
-      3. Limit User Privileges on the Computer. Always access Internet as a Standard user and not as Administrator.
-      4. Check and verify email sender IDs and web links before opening file attachments and clicking on links in emails and webpages.
-      5. Phishing emails and SMS are used to get user credentials like username,password cerdit card and PIN numbers etc. Be wary of them.
-      6. Regularly check the last logging details of email accounts.
-      7. Use strong passwords and different passwords for diffrenet online services.
-      8. Regularly take backup of document files to avoid loss of files in case of emergencies like malware infections, hard disk crash,corrupted applications and other unforeseen incidents.
-      
-          Tips for Safe Online Chatting
-      1. Position the computer in your main living space and make sure the monitor faces OUTWARD into the room so there is no secrecy.THis is the single MOST valuable thing you can do for your childs safety online.
-      2. Work as a team to set your boundaries. Discuss with your child exactly what is OK and what is not OK regrading what kind of web sites are appropriate for thwm to visit, which chat rooms to visit and what kinds of things to talk about there.Set logical consequence for when your child disregards your rulles(like grounded from the Internet for 1 week), but do NOT threaten to ban the Internet forever.
-          ''',
-      style: TextStyle(fontSize: 13),
-    ),
-    ),
+        centerTitle: true,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Text(
+                'Cyber Security Norms and Best Practices',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                '''
+1. Always use genuine software.
+2. Install the latest updates/patches for the operating system, Antivirus, and Application software.
+3. Limit User Privileges on the Computer. Always access Internet as a Standard user and not as Administrator.
+4. Check and verify email sender IDs and web links before opening file attachments and clicking on links in emails and webpages.
+5. Phishing emails and SMS are used to get user credentials like username, password, credit card, and PIN numbers etc. Be wary of them.
+6. Regularly check the last logging details of email accounts.
+7. Use strong passwords and different passwords for different online services.
+8. Regularly take backup of document files to avoid loss of files in case of emergencies like malware infections, hard disk crash, corrupted applications, and other unforeseen incidents.''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Tips for Safe Online Chatting',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                '''
+1. Position the computer in your main living space and make sure the monitor faces OUTWARD into the room so there is no secrecy. This is the single MOST valuable thing you can do for your child's safety online.
+2. Work as a team to set your boundaries. Discuss with your child exactly what is OK and what is not OK regarding what kind of websites are appropriate for them to visit, which chat rooms to visit, and what kinds of things to talk about there. Set logical consequences for when your child disregards your rules (like grounded from the Internet for 1 week), but do NOT threaten to ban the Internet forever.
+3. Stress to your child that they need to tell you if they get any weird or upsetting messages while chatting, and that you will not be angry with them nor will you ban the internet as a result. Make it clear to the child that you understand that the child cannot control what other people say to him or her and that they are not to be blamed if this happens.
+4. Set strict time limits for internet chat use and enforce them. Internet addiction is a real thing.
+5. Make it clear to your child that people in chat rooms are ALWAYS strangers, no matter how often they chat to them, and no matter how well they think they know them, and that while they may be good or bad people, they are still strangers. Your child should therefore not always believe everything people say in chat rooms or online.
+6. Make sure your child understands that they are never to tell a person online their real name, their school, their phone number, or where they live.
+7. Do not permit your child to be left alone in cyberspace for long periods of time - this is when they are most vulnerable. Make sure that their chat time occurs when YOU are around in the house so that you can check in on them regularly.
+8. Be sure to stress to your child that they are to behave politely and respectfully at all times while chatting online with strangers or sending email to friends.
+9. Don't panic! No one can harm your child through the Internet as long as your child follows your rules.
+10. Take an active interest in your child's activity online. Do NOT use the Internet as a babysitter. Learn to surf the Web and chat online yourself so you understand what it is that your child is doing. If you don't know how to chat online, ask your child to teach you!''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
         ),
+      ),
     );
   }
 }
--- lib/citizen/services pages/information services/internet.dart
+++ lib/citizen/services pages/information services/internet.dart
@@ -6,10 +6,11 @@ class InternetPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF90CAF9),
-        title: Text('Internet Tips',
-          style:TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Color(0xFFF8F8F8)
+        title: Text(
+          'Internet Tips',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFF8F8F8),
           ),
         ),
         centerTitle: true,
@@ -17,23 +18,125 @@ class InternetPage extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(4.0),
+        padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Text(
-            '''
-            Over confidence is not a good thing.
-     
-     You may be a person who is enthusiastic about internet and not keen on internet aware of dangers in the cyber world and who takes precautions to protect privacy and sensitive data. However do you know anything in detail about the new friends you made in the cyber world, anything about the group or communities you joined or about a person in the cyber world who you think as a good man ?. Will you inform your parents/wife/husband/friend that you are going to meet a person who is a friend of you in the cyber world? Think about it.
-       
-          Do not share your private information with anyone the cyber world.
-     
-     Chances are there that a cordial relationship of today will become enmity tomorrow. The photos, mails, chats, and videos etc you shared with a person while you were in friendship may be used against you after your break up. The information you share in the cyber world may be used against you by your jealous friend, enemy or by a criminal. 
-       
-          Do not share your password with anyone. 
-     
-     Do not share your password and other sensitive information with your friends, is blind belief knowingly or unknowingly your friend may use your credentials against you .
-            ''',
-            style: TextStyle(fontSize: 13),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Text(
+                'Over confidence is not a good thing.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '''
+You may be a person who is enthusiastic about the internet and not keen on internet awareness of dangers in the cyber world and who takes precautions to protect privacy and sensitive data. However, do you know anything in detail about the new friends you made in the cyber world, anything about the groups or communities you joined or about a person in the cyber world who you think is a good person? Will you inform your parents/wife/husband/friend that you are going to meet a person who is a friend of yours in the cyber world? Think about it.   ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Do not share your private information with anyone in the cyber world.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('''    
+ Chances are there that a cordial relationship of today will become enmity tomorrow. The photos, mails, chats, and videos, etc., you shared with a person while you were in friendship may be used against you after your breakup. The information you share in the cyber world may be used against you by your jealous friend, enemy, or by a criminal.   ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Do not share your password with anyone.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('''    
+ Do not share your password and other sensitive information with your friends, is blind belief knowingly or unknowingly your friend may use your credentials against you.  ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Unplug or Close webcam when it is not in use',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('''   
+ Softwares that can spy on you by recording your video are available now. These softwares can start themselves and can record your video without your permission and knowledge. So always unplug or close webcams when not in use.   ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Do not allow others to download pirated materials by using your computer',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('''     
+ You may be well aware of the consequence of downloading pirated materials on your computer or mobile phone. If you share your computer or mobile with others, chances are there that they may access it in the future without permission. They may access your emails and notes, etc. So always secure your computer or mobile phones, etc., by using passwords.  ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Use antivirus/Security software.',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text('''     
+Use updated antivirus or Security software. Do not show laziness in installing or updating antivirus/security software. Do the antivirus software updating periodically. Use antivirus/security software after ensuring credibility and authenticity. ''',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Internet Tips',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue, // Highlighted color
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(''' 
+ 1. Try to visit/use lawful websites only.
+ 2. Sites which use https protocol have better security than sites which use http protocol so expect some security flaws while browsing websites which use http.
+ 3. Do not disclose your password to anyone
+ 4. Do not open email from strangers
+ 5. Give importance to privacy and security of others as yours
+ 6. Do not believe blindly in the friendship and relationships in the cyber world
+ 7. Do not share personal, family, and banking details with strangers
+ 8. Inform parents/friends/authorities if someone sends inappropriate or irritating mails to you
+ 9. Refuse the invitations from online friends to meet somewhere.
+ 10. Do not use copyrighted materials from the internet without prior permission
+ 11. Do not click on suspicious links
+ 12. Free software/mobile applications may contain malwares.
+                
+                 ''',
+                style: TextStyle(fontSize: 16),
+              ),
+
+              SizedBox(height: 20),
+            ],
           ),
         ),
       ),
